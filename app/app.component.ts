7e23fc34b7c3c99294a9ee8a2d73a72db1201786
Mtorre18 <35480065+Mtorre18@users.noreply.github.com>
2019-05-03 13:17:20 -0500

    First Venture App Commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css']
+  template: '<h1>My App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {}
